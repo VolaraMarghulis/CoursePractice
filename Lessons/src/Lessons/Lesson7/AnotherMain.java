30c8cb3e0b7a9bf75eb9f0c4e797e4efc16874fa
Valeriu <valeriu.juc99@gmail.com>
2025-03-14 18:36:10 +0300

    add content

--- Lessons/src/Lessons/Lesson7/AnotherMain.java
+++ Lessons/src/Lessons/Lesson7/AnotherMain.java
@@ -1,4 +1,29 @@
 package Lessons.Lesson7;
+import java.util.Scanner;
 
 public class AnotherMain {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    //TODO 2. Write a program to print numbers from 1 to 10.
+        for (int i = 1 ; i <= 10; i++){
+            System.out.print(i + " ");
+        }
+    //TODO 3.Write a program that prompts user for a positive integer.
+    // The program shall read the input as int; and print the "reverse" of the input integer.
+        System.out.println();
+        System.out.print("Input the number to reverse: ");
+        int number = sc.nextInt();
+
+        int reversedNumber = 0;
+        while (number > 0){
+            //use to take the last number
+            // 12345 % 10 = 5
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            //use to remove last digit
+            //12345 / 10 = 1234
+            number /= 10;
+        }
+        System.out.println(reversedNumber);
+    }
 }
--- Lessons/src/Lessons/Lesson7/Convertor.java
+++ Lessons/src/Lessons/Lesson7/Convertor.java
@@ -1,5 +1,39 @@
 package Lessons.Lesson7;
+import java.util.Scanner;
+public class Convertor {
+    //TODO 1.Write a program that prompts the user to input a currency code (e.g., USD, EUR, GBP) and a value in USD.
+    // The program should convert that value into the corresponding currency. Assume a few conversion rates as follows:
+    enum Currency {
+        USD(1),
+        EUR(0.85), GBP(0.75),
+        INR(75.00), AUD(1.35);
+        private final double rateToUSD;
 
-public class Lesson7 {
-    
+        Currency(double rateToUSD) {
+            this.rateToUSD = rateToUSD;
+        }
+        public double getRateToUSD() {
+            return rateToUSD;
+        }
+        // Convert method
+        public double convertTo(Currency targetCurrency, double amount) {
+            double amountInUSD = amount / this.rateToUSD;
+            return amountInUSD * targetCurrency.getRateToUSD();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double amountInUSD = sc.nextDouble();
+        double amountInEUR = Currency.USD.convertTo(Currency.EUR, amountInUSD);
+        double amountInGBP = Currency.USD.convertTo(Currency.GBP, amountInUSD);
+        double amountInINR = Currency.USD.convertTo(Currency.INR, amountInUSD);
+        double amountInAUD = Currency.USD.convertTo(Currency.AUD, amountInUSD);
+
+        System.out.println(amountInUSD + " USD is " + amountInEUR + " EUR");
+        System.out.println(amountInUSD + " USD is " + amountInGBP + " GBP");
+        System.out.println(amountInUSD + " USD is " + amountInINR + " INR");
+        System.out.println(amountInUSD + " USD is " + amountInAUD + " AUD");
+    }
 }
+
+
